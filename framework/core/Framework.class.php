d7e287a4a49eaed70bdba70c68175b49339ca430
tmwalo <vuyotm@gmail.com>
2017-11-09 01:03:22 +0200

    add core framework functionality

---
+++ framework/core/Framework.class.php
@@ -0,0 +1,66 @@
+<?php
+
+abstract class Framework {
+
+  private static $allowedControllersAndActions = array(
+    'pages' => ['home', 'error'],
+  );
+
+  private static function setErrorReporting()
+  {
+    if (DEVELOPMENT_ENV == true) {
+      error_reporting(E_ALL);
+      ini_set('display_errors', 'On');
+    }else {
+      ini_set('display_errors', 'Off');
+    }
+  }
+
+  private static function autoloadController($classname)
+  {
+    require_once(CONTROLLER_PATH . "$classname.class.php");
+  }
+
+  private static function autoloadModel($classname)
+  {
+    require_once(MODEL_PATH . "$classname.class.php");
+  }
+
+  private static function autoload()
+  {
+    //spl_autoload_register('autoloadController');
+    //spl_autoload_register('autoloadModel');
+  }
+
+  private static function dispatch()
+  {
+    if (isset($_GET['controller']) && isset($_GET['action'])) {
+      $controller = $_GET['controller'];
+      $action = $_GET['action'];
+    }
+    else {
+      $controller = 'pages';
+      $action = 'home';
+    }
+
+    if ( (!array_key_exists($controller, self::$controllersAndActions)) || (!in_array($action, self::$controllersAndActions[$controller])) ) {
+      $controller = 'pages';
+      $action = 'home';
+    }
+
+    $capitalizeController = ucwords($controller);
+    $controllerClassName = $capitalizeController . "Controller";
+    $controller = new $controllerClassName();
+    $controller->$action();
+  }
+
+  public static function run()
+  {
+    self::setErrorReporting();
+    self::autoload();
+    self::dispatch();
+  }
+
+}
+
+?>
---
+++ framework/database/DbSchema.class.php
@@ -0,0 +1,97 @@
+<?php
+
+require_once(INCLUDES_PATH . 'connection.inc.php');
+
+abstract class DbSchema {
+
+    private static function createDatabaseCamagru(PDO $db)
+    {
+      $sql = "CREATE DATABASE IF NOT EXISTS camagru";
+      try {
+        $db->exec($sql);
+      } catch (PDOException $e) {
+        echo "Database error: " . $e->getMessage() . "<br />";
+      }
+    }
+
+    private static function useDatabaseCamagru(PDO $db)
+    {
+      $sql = "USE camagru";
+      try {
+        $db->exec($sql);
+      } catch (PDOException $e) {
+        echo "Database error: " . $e->getMessage() . "<br />";
+      }
+    }
+
+    private static function createUsersTable(PDO $db)
+    {
+      $sql = "CREATE TABLE IF NOT EXISTS users (
+                user_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                username VARCHAR(20) NOT NULL,
+                email VARCHAR(60) NOT NULL,
+                pword CHAR(20) NOT NULL,
+                registration_date DATETIME NOT NULL,
+                PRIMARY KEY (user_id)
+              ) ENGINE = INNODB";
+      try {
+        $db->exec($sql);
+      } catch (PDOException $e) {
+        echo "Database error: " . $e->getMessage() . "<br />";
+      }
+    }
+
+    private static function createImagesTable(PDO $db)
+    {
+      $sql = "CREATE TABLE IF NOT EXISTS images (
+                image_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                user_id INT UNSIGNED NOT NULL,
+                filename VARCHAR(20) NOT NULL,
+                likes INT UNSIGNED NOT NULL DEFAULT 0,
+                upload_date DATETIME NOT NULL,
+                PRIMARY KEY (image_id),
+                FOREIGN KEY (user_id) REFERENCES users (user_id)
+              ) ENGINE = INNODB";
+      try {
+        $db->exec($sql);
+      } catch (PDOException $e) {
+        echo "Database error: " . $e->getMessage() . "<br />";
+      }
+    }
+
+    private static function createCommentsTable(PDO $db)
+    {
+      $sql = "CREATE TABLE IF NOT EXISTS comments (
+                comment_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                parent_id INT UNSIGNED NOT NULL DEFAULT 0,
+                image_id INT UNSIGNED NOT NULL,
+                user_id INT UNSIGNED NOT NULL,
+                comment VARCHAR(255) NOT NULL,
+                likes INT UNSIGNED NOT NULL DEFAULT 0,
+                submit_date DATETIME NOT NULL,
+                PRIMARY KEY (comment_id),
+                FOREIGN KEY (image_id) REFERENCES images (image_id)
+                ON DELETE CASCADE,
+                FOREIGN KEY (user_id) REFERENCES users (user_id)
+              ) ENGINE = INNODB";
+      try {
+        $db->exec($sql);
+      } catch (PDOException $e) {
+        echo "Database error: " . $e->getMessage() . "<br />";
+      }
+    }
+
+    public static function createDatabase()
+    {
+      $db = Db::getInstance();
+
+      self::createDatabaseCamagru($db);
+      self::useDatabaseCamagru($db);
+      self::createUsersTable($db);
+      self::createImagesTable($db);
+      self::createCommentsTable($db);
+    }
+
+}
+
+?>
