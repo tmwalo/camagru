df13b15dfca7519a4f56c943ff5271ef76f4d806
tmwalo <vuyotm@gmail.com>
2017-11-09 00:56:33 +0200

    add web app home and rewrite all requests as home

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+  define('DEVELOPMENT_ENV', 'true');
+
+  define('DS', DIRECTORY_SEPARATOR);
+  define('ROOT', dirname(__FILE__) . DS);
+  define('APP_PATH', ROOT . 'application' . DS);
+  define('CONTROLLER_PATH', APP_PATH . 'controllers' . DS);
+  define('MODEL_PATH', APP_PATH . 'models' . DS);
+  define('CONFIG_PATH', ROOT . 'config' . DS);
+  define('FRAMEWORK_PATH', ROOT . 'framework' . DS);
+  define('CORE_PATH', FRAMEWORK_PATH . 'core' . DS);
+  define('DATABASE_PATH', FRAMEWORK_PATH . 'database' . DS);
+  define('INCLUDES_PATH', ROOT . 'includes' . DS);
+
+  require_once(CONFIG_PATH . 'setup.php');
+
+  require_once(CORE_PATH . 'Framework.class.php');
+  Framework::run();
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>test</title>
+  </head>
+  <body>
+    <h1>Camagru</h1>
+  </body>
+</html>
