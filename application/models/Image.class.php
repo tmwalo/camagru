10f04ab34d292df0d3b9c94a25d72ca889530d97
tmwalo <vuyotm@gmail.com>
2017-11-19 11:03:39 +0200

    add home page gallery

--- application/models/Image.class.php
+++ application/models/Image.class.php
@@ -9,6 +9,13 @@
       return ($stmt);
     }
 
+    public static function countAll($pdo)
+    {
+      $sql = "SELECT COUNT(image_id) AS total FROM images";
+      $stmt = $pdo->query($sql);
+      return ($stmt);
+    }
+
     public static function find($pdo, $image_id)
     {
       $sql = "SELECT * FROM images WHERE image_id = ?";
@@ -28,6 +35,20 @@
       $sql = "SELECT * FROM images WHERE user_id = ?";
       $stmt = $pdo->prepare($sql);
       $stmt->bindValue(1, $user_id, PDO::PARAM_INT);
+      $result = $stmt->execute();
+      if ($result)
+        return ($stmt);
+      else
+        return (false);
+    }
+
+    public static function findRange($pdo, $order_by, $start_index, $num_records)
+    {
+      $sql = "SELECT * FROM images ORDER BY ? LIMIT ?, ?";
+      $stmt = $pdo->prepare($sql);
+      $stmt->bindValue(1, $order_by, PDO::PARAM_STR);
+      $stmt->bindValue(2, $start_index, PDO::PARAM_INT);
+      $stmt->bindValue(3, $num_records, PDO::PARAM_INT);
       $result = $stmt->execute();
       if ($result)
         return ($stmt);
--- application/views/home.php
+++ application/views/home.php
@@ -1,10 +1,90 @@
 <?php
   $title = "Camagru Home";
   require_once(INCLUDES_VIEWS . 'header_top.inc.php');
+?>
+<link rel="stylesheet" href="css/home.css">
+<?php
   require_once(INCLUDES_VIEWS . 'header_bottom.inc.php');
 ?>
 <h1>Camagru Home Page</h1>
 <h2>Gallery:</h2>
+<div class="gallery">
+<?php
+
+  require_once(INCLUDES_DATABASE . "connection.inc.php");
+  require_once(MODEL_PATH . "Image.class.php");
+
+  $records_per_page = 3;
+  $pdo = Db::getInstance();
+
+  $result = Image::countAll($pdo);
+  if ($result) {
+    $stmt = $result;
+    $row = $stmt->fetch();
+    $records = $row['total'];
+    if ($records > $records_per_page)
+      $pages = ceil($records / $records_per_page);
+    else
+      $pages = 1;
+  }
+  else
+    return ;
+
+  if (isset($_GET['s']) && is_numeric($_GET['s']) )
+    $start_index = (int) $_GET['s'];
+  else
+    $start_index = 0;
+
+  $result = NULL;
+  $stmt = NULL;
+  $row = NULL;
+
+  $order_by = "upload_date DESC";
+  var_dump($order_by);
+  var_dump($start_index);
+  var_dump($records_per_page);
+  $result = Image::findRange($pdo, $order_by, $start_index, $records_per_page);
+  if ($result) {
+    $stmt = $result;
+    while ($row = $stmt->fetch() ) {
+      echo "<div class=\"gallery_img\">
+              <img src=\"uploads/" . $row['filename'] . "\" alt=\"\" />
+            </div>";
+    }
+  }
+
+  if ($pages > 1) {
+
+    echo "<br />
+          <div class=\"pagination\">";
+
+    $current_page = ($start_index / $records_per_page) + 1;
+
+    if ($current_page != 1) {
+      echo "<a href=\"index.php?s=" . urlencode(($start_index - $records_per_page) ) . "\">Previous </a>";
+    }
+
+    $index = 1;
+    while ($index <= $pages) {
+      if ($index != $current_page) {
+        echo "<a href=\"index.php?s=" . urlencode(($index - 1) * $records_per_page) . "\">" . $index . "</a>";
+      }
+      else {
+        echo " $index ";
+      }
+      $index++;
+    }
+
+    if ($current_page != $pages) {
+      echo "<a href=\"index.php?s=" . urlencode(($start_index + $records_per_page) ) . "\"> Next</a>";
+    }
+
+    echo "</div>";
+
+  }
+
+?>
+</div>
 <?php
   require_once(INCLUDES_VIEWS . 'footer_top.inc.php');
   require_once(INCLUDES_VIEWS . 'footer_bottom.inc.php');
